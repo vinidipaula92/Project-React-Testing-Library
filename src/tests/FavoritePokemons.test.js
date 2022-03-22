1eb8cbcb9959f4154dc86c452a030c3eebefec01
vinidipaula <vinidipaula92@gmail.com>
2022-03-22 17:27:55 -0300

    Requisito 4 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente FavoritePokemons.js', () => {
+  it('Será exibido mensagem no favorite pokemon found se não tiver pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const favorite = screen.getByText(/No favorite pokemon found/i);
+    expect(favorite).toBeDefined();
+  });
+/*   it('Teste se é exibido todos os cards de pokémons favoritados.', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const favoritePokemon = screen.getByRole();
+  }); */
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente NotFound.js', () => {
+  it('Teste se página contém heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const fav = screen.getByRole('heading', { name: /Page requested not/i, level: 2 });
+    expect(fav).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem Pikachu chorando 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const picture = screen.getByAltText(/Pikachu crying because the page requested/i);
+    expect(picture).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
