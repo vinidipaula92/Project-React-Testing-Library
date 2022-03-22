4c5dc8bd3f0e5c4249f82b8217172b44e92a46f9
vinidipaula <vinidipaula92@gmail.com>
2022-03-22 15:39:49 -0300

    Requisito 1 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente App.js', () => {
+  it('Teste se o link possui o texto home e é redirecionado ao /home', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+    userEvent.click(home);
+  });
+  it('Teste se o link possui o texto about e é redirecionado ao /about', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+    userEvent.click(about);
+  });
+  it('Teste o link possui o texto Favorite Pokémons e redirecionado ao /favorite', () => {
+    renderWithRouter(<App />);
+    const favorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favorite).toBeInTheDocument();
+    userEvent.click(favorite);
+  });
+  it('Teste o link possui o texto home e é redirecionado ao /home', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notFound).toBeInTheDocument();
+  });
+});
