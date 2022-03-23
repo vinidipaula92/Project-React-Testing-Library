a7533676aa5ac6e5219cb1f7d356a14049e667ce
vinidipaula <vinidipaula92@gmail.com>
2022-03-22 23:05:22 -0300

    Requisito 6 iniciado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente Pokemon.js', () => {
+  it('Teste se é renderizado um card com as informações do pokémon.', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeDefined();
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toBeDefined();
+    const pokemonAverage = screen.getByTestId('pokemon-weight');
+    expect(pokemonAverage).toBeDefined();
+    const pokemonImage = screen.getByAltText(/sprite/i);
+    expect(pokemonImage).toBeDefined();
+  });
+  it('Teste se tem o link com os detalhes do pokémon.', () => {
+    renderWithRouter(<App />);
+    const linkDetails = screen.getByRole('link', { name: /more details/i });
+    expect(linkDetails).toBeInTheDocument();
+  });
+});
