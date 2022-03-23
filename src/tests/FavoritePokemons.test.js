585aaeacc44d1d30bcd32819dfec2ce96674f600
vinidipaula <vinidipaula92@gmail.com>
2022-03-23 17:04:06 -0300

    Requisito 7 finalizado e o requisito 3 refatorado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
@@ -10,12 +11,14 @@ describe('Teste o componente FavoritePokemons.js', () => {
     const favorite = screen.getByText(/No favorite pokemon found/i);
     expect(favorite).toBeDefined();
   });
-/*   it('Teste se é exibido todos os cards de pokémons favoritados.', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue(renderFavoritePokemon),
-    });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', async () => {
     const { history } = renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+    const checked = screen.getByText(/pokémon favoritado?/i);
+    userEvent.click(checked);
     history.push('/favorites');
-    const favoritePokemon = screen.findByText();
-  }); */
+    const favoritePokemon = screen.getByTestId('pokemon-name').textContent;
+    expect(favoritePokemon).toBeDefined();
+  });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente PokemonDetails.js', () => {
+  it('Teste se as informações do Pokémon selecionado são mostradas na tela.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/65');
+    const details = screen
+      .getByRole('heading', { name: /Alakazam Details/i, level: 2 });
+    expect(details).toBeDefined();
+    const summary = screen
+      .getByRole('heading', { name: /summary/i, level: 2 });
+    expect(summary).toBeDefined();
+    const pokemonResume = screen
+      .getByText(/Closing both its eyes heightens all its other senses/i);
+    expect(pokemonResume).toBeDefined();
+  });
+  it('Teste se tem uma seção com os mapas contendo as localizações do pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/65');
+    const detailsMap = screen
+      .getByRole('heading', { name: /Game Locations of Alakazam/i, level: 2 });
+    expect(detailsMap).toBeDefined();
+    const mapLocation = screen.getAllByRole('img')[1];
+    expect(mapLocation.src).toBe('https://cdn2.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png');
+    const mapName = screen.getByText(/Unova Accumula Town/i);
+    expect(mapName).toBeDefined();
+    const pokemonName = screen.getByTestId('pokemon-name').textContent;
+    expect(mapLocation.alt).toBe(`${pokemonName} location`);
+  });
+  it('Teste se o usuário pode favoritar um pokémon através da página detalhes.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/65');
+    const checked = screen.getByText(/pokémon favoritado?/i);
+    expect(checked).toBeDefined();
+    userEvent.click(checked);
+  });
+});
